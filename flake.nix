30c999340d50b9e926322eed9c9aea110e930e02
Bryant Paz <pazbryant7@outlook.com>
2026-07-16 21:41:00 -0600

    feat(nix): adding a new flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Full-stack dev environment (replaces Mason toolchain)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            android-tools
+            just
+            typos
+          ];
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
+
